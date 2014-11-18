271f3bafb9cbfe1805aca533c20ad042f54cbb0f
Elfhir <ta.thanhliem@gmail.com>
2014-11-18 02:44:57 +0100

    Improve design : scrollnav, flip card

--- app/scripts/controllers/UI/anchorscroll.js
+++ app/scripts/controllers/UI/anchorscroll.js
@@ -6,22 +6,20 @@
 
 	/**
 	 * @ngdoc function
-	 * @name marcopoloApp.controller:NavigationCtrl
+	 * @name marcopoloApp.controller:AnchorScrollCtrl
 	 * @description
-	 * # HomeCtrl
-	 * Controller of the marcopoloApp doing the changement of color on links
-	 * during navigation
+	 * # AnchorScrollCtrl
+	 * Controller of the marcopoloApp doing the changement smooth croll on anchors navigation
+	 * 
 	 */
 	marcopoloApp.controller('AnchorScrollCtrl', function($scope, $document){
 		$scope.toTheTop = function() {
-			$document.scrollTopAnimated(0).then(function() {
-				console && console.log('You just scrolled to the top!');
-			});
+			$document.scrollTopAnimated(0);
 		};
 		var section2 = angular.element(document.getElementById('section-2'));
 		$scope.toSection2 = function() {
 			$document.scrollToElementAnimated(section2);
-		}
+		};
 	});
 
 	marcopoloApp.value('duScrollOffset', 30);
